cc5e4235078fa9de8b3f9f72eebb56c86e101c57
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-02-23 15:32:26 +0800

    圆测试类

---
+++ src/day_2024223/RoundTest.java
@@ -0,0 +1,8 @@
+package day_2024223;
+
+public class RoundTest {
+    public static void main(String[] args) {
+        Round round1 = new Round();
+        System.out.println(round1.area());
+    }
+}
