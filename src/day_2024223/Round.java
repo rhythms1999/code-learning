50eac8673154231f372558669777a41d70f133fd
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-02-23 15:48:16 +0800

    OOP 添加圆的无参构造方法
    源代码没写构造方法时，当被编译成.class文件时会自动生成

--- src/day_2024223/Round.java
+++ src/day_2024223/Round.java
@@ -1,9 +1,15 @@
 package day_2024223;
 
 public class Round {
+    //state
     double r=2;
     double pi=3.14;
 
+    //构造方法
+    public Round() {
+    }
+
+    //behavior
     public double area(){
         return r*r*pi;
     }
