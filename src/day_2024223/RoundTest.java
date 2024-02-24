ef3c95c6ffb19dc873b78b9988a90968f0bd77b6
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-02-24 20:57:03 +0800

    ClassName object = new ClassName(arguments);

--- src/day_2024223/RoundTest.java
+++ src/day_2024223/RoundTest.java
@@ -2,7 +2,7 @@ package day_2024223;
 
 public class RoundTest {
     public static void main(String[] args) {
-        Round round1 = new Round();
+        Round round1 = new Round(2);
         System.out.println(round1.area());
     }
 }
