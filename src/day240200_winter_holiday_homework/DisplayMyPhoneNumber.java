08cef172b913bf6872c7149932fd2364c2c670b7
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-02-26 21:54:59 +0800

    俩队数组输出自己的手机号码

---
+++ src/day240200_winter_holiday_homework/DisplayMyPhoneNumber.java
@@ -0,0 +1,13 @@
+package day240200_winter_holiday_homework;
+
+public class DisplayMyPhoneNumber {
+    public static void main(String[] args) {
+
+        int arr1[] = new int[]{0, 1, 2, 3, 5, 6, 8, 9};
+        int arr2[] = new int[]{1, 3, 5, 6, 4, 4, 7, 0, 5, 3, 2};
+
+        for (int i = 0; i<arr2.length;i++){
+            System.out.print(arr1[arr2[i]]);
+        }
+    }
+}
