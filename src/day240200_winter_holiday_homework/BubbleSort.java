833d163ad984b6d678eecf151edbeb5d77675acc
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-02-27 17:18:17 +0800

    冒泡排序step2-找出第二大的数字将其排在最大数字的前面 相较于第一次排序，因为已经将最大的数字排好了顺序，所以相较于第一次的循环顺序可在基础上再做-1即为（i<arr.length-1-1）

--- src/day240200_winter_holiday_homework/BubbleSort.java
+++ src/day240200_winter_holiday_homework/BubbleSort.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 public class BubbleSort {
      public static void main(String[] args) {
-         int[] arr = new int[]{28, 2, 5, 9, 11};
+         int[] arr = new int[]{28, 5, 11, 2, 4};
          int temp;
+         System.out.println("未经过排序的数组");
+         System.out.println(Arrays.toString(arr));
+         System.out.println("------------");
 
          for (int i = 0;i<arr.length-1;i++)
          {
@@ -16,6 +19,21 @@ public class BubbleSort {
                  arr[i+1]=temp;
              }
          }
+         System.out.println("经过一轮排序后的数组");
          System.out.println(Arrays.toString(arr));
+         System.out.println("------------");
+
+         for (int i = 0;i<arr.length-1-1;i++)
+         {
+             if (arr[i]>arr[i+1])
+             {
+                 temp=arr[i];
+                 arr[i]=arr[i+1];
+                 arr[i+1]=temp;
+             }
+         }
+         System.out.println("经过二轮排序后的数组");
+         System.out.println(Arrays.toString(arr));
+         System.out.println("------------");
      }
 }
