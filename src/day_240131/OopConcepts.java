24e01a1b8e270f013a875cc35da381f433dc90dd
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-02-27 19:29:36 +0800

    oop的一些概念

---
+++ src/day_240131/OopConcepts.java
@@ -0,0 +1,77 @@
+package day_240131;
+/**
+ * Object-Oriented Programming
+ * OOP
+ * 面向对象编程 VS 面向过程编程
+ *
+ */
+public class OopConcepts {
+    public static void main(String[] args) {
+        /**
+         * If you've never used an object-oriented programming language before,
+         * you'll need to learn a few basic concepts before you can begin writing any code.
+         *
+         * What Is an Object?
+         * - An object is a software bundle of related state and behavior.
+         * - Software objects are often used to model the real-world objects that you find in everyday life.
+         * What Is a Class?
+         * - A class is a blueprint or prototype from which objects are created.
+         */
+        String link1 = "https://docs.oracle.com/javase/tutorial/java/concepts/index.html";
+
+
+        /**
+         * Objects are key to understanding object-oriented technology.
+         * Look around right now and you'll find many examples of real-world objects:
+         * your dog, your desk, your television set, your bicycle.
+         *
+         * Real-world objects share two characteristics: They all have state and behavior.
+         * - Dogs have state (name, color, breed, hungry) and behavior (barking, fetching, wagging tail).
+         * - Bicycles also have state (current gear, current pedal cadence, current speed) and
+         * behavior (changing gear, changing pedal cadence, applying brakes).
+         *
+         * An object stores its state in fields (variables in some programming languages)
+         * and exposes its behavior through methods (functions in some programming languages).
+         */
+        String link2 = "https://docs.oracle.com/javase/tutorial/java/concepts/object.html";
+
+
+        /**
+         * by location
+         *      成员变量 Fields
+         *        实例变量 Instance Variables (Non-Static Fields)
+         *        类变量 Class Variables (Static Fields)
+         *      局部变量 Local Variables
+         *        方法内
+         *        代码块内
+         *        方法参数 Parameters
+         */
+        String link3 = "https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html";
+        System.out.println(link1);
+        System.out.println(link2);
+        System.out.println(link3);
+
+        /*
+         * Q:
+         * 理解OOP
+         * 面向对象编程 ___
+         *
+         * A.
+         * 是一个新的编程语言
+         *
+         * B.
+         * 是方法的另一种叫法
+         *
+         * C.
+         * 不涉及创建对象和类
+         *
+         * D.
+         * 是一种通过创建对象来解决问题的技术
+         *
+         */
+
+        /*
+        In OOP, we create objects to solve complex problems.
+         */
+    }
+}
