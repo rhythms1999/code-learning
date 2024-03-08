e27d137740b8012bc5adab22cc1ea5671b999040
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-08 16:34:24 +0800

    构造器：权限修饰符+类名形成最简单的无参构造

---
+++ src/day_240306/Student.java
@@ -0,0 +1,15 @@
+package day_240306;
+
+public class Student {
+    int age;
+    String name;
+    int high;
+
+    public Student(int age){
+
+    }
+
+    public Student(int age,String name){
+
+    }
+}
