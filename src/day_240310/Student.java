6aa8729cf40fb167655a1e0bb724a60214195c18
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-10 10:58:52 +0800

    singleton basic

---
+++ src/day_240310/Student.java
@@ -0,0 +1,9 @@
+package day_240310;
+
+public class Student {
+    String name;
+    public Student() {
+        System.out.println("-------------");
+    }
+
+}
---
+++ src/day_240310/StudentTest.java
@@ -0,0 +1,11 @@
+package day_240310;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println(student1);
+
+        Student student2 = new Student();
+        System.out.println(student2);
+    }
+}
