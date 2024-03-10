b94b8d7313a85eaf0fe3d85b9b7054962c031f39
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-10 11:46:50 +0800

    singleton basic- private - 在 state new (err)
    在类的内部 -> 内部哪里？
    - state ->
    Student student = new Student();

--- src/day_240310/Student.java
+++ src/day_240310/Student.java
@@ -1,9 +1,11 @@
 package day_240310;
 
 public class Student {
+    //state
     String name;
+    Student student = new Student();
     private Student() {
         System.out.println("-------------");
     }
-
+    //behavior
 }
--- src/day_240310/StudentTest.java
+++ src/day_240310/StudentTest.java
@@ -7,5 +7,13 @@ public class StudentTest {
 
         Student student2 = new Student();
         System.out.println(student2);
+
+        // 要求是：“Student 类只能有一个实例对象”！该怎么做？
+        // 思路
+        //   1. 把构造器私有化 -> 问题 -> 外部不能 new -> 在哪里 new?
+        //   2. 在类的内部 -> 内部哪里？
+        //        - state -> Student student = new Student();
+        //        - behavior
+        Student student = new Student();
     }
 }
