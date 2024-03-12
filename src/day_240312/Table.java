2abbeda67b2adda0ac56daec3bc165dc084ee267
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-12 17:12:48 +0800

    my test：singleton basic-step-1
    初次运行结果可以看到俩次打印的desk1和2的地址是俩个不同的值，同时可以看到在TableTest中俩次new出的desk是重复俩次调用了构造器，因为sout结果被打印了俩次。
    
    下一步：如何让Table类的实例对象只能有一个？

---
+++ src/day_240312/Table.java
@@ -0,0 +1,14 @@
+package day_240312;
+
+public class Table {
+    //state
+
+
+    //constructor
+    public Table(){
+        System.out.println("构造器调用");
+    }
+
+    //behavior
+
+}
---
+++ src/day_240312/TableTest.java
@@ -0,0 +1,11 @@
+package day_240312;
+
+public class TableTest {
+    public static void main(String[] args) {
+        Table desk1 = new Table();
+        System.out.println(desk1);
+
+        Table desk2 = new Table();
+        System.out.println(desk2);
+    }
+}
