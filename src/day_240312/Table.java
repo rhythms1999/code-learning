342f70f44658d28e6ffbade45b357e53fd302605
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-12 17:44:18 +0800

    my test：singleton basic-step-2
    将构造器私有化，在外部无法实例化对象，只能在内部(state中，还可以尝试在behavior中)new出一个课桌实例（deskinstance），引出static关键字，它属于类，不需要new出对象但是能通过Table desk = Table.deskinstance的方式进行访问。同时运行main程序可以看到虽然有俩个desk但是打印出的都是同一地址，这表明他们本质上其实是同一个。但是在这一步中deskinstance的值是可以被改变的。
    
    下一步：如何让deskinstance的值不可用被更改？

--- src/day_240312/Table.java
+++ src/day_240312/Table.java
@@ -2,10 +2,10 @@ package day_240312;
 
 public class Table {
     //state
-
+    static Table deskinstance = new Table();
 
     //constructor
-    public Table(){
+    private Table(){
         System.out.println("构造器调用");
     }
 
--- src/day_240312/TableTest.java
+++ src/day_240312/TableTest.java
@@ -2,10 +2,10 @@ package day_240312;
 
 public class TableTest {
     public static void main(String[] args) {
-        Table desk1 = new Table();
+        Table desk1 = Table.deskinstance;
         System.out.println(desk1);
 
-        Table desk2 = new Table();
+        Table desk2 = Table.deskinstance;
         System.out.println(desk2);
     }
 }
