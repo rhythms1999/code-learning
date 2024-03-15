0d04ee2d89228b4c4d5549841657f99c491eb701
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-15 20:12:36 +0800

    递归-1加到100

---
+++ src/day_240315/AddtoHundredMethodTwo.java
@@ -0,0 +1,19 @@
+package day_240315;
+
+public class AddtoHundredMethodTwo {
+    static int sum;
+    int j = 1;
+
+    public int getAddtoHundred(int i) {
+        if (j <= i) {
+            sum = sum + j++;
+            getAddtoHundred(100);
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        AddtoHundredMethodTwo AddtoHunred = new AddtoHundredMethodTwo();
+        AddtoHunred.getAddtoHundred(100);
+        System.out.println(sum);
+    }
+}
