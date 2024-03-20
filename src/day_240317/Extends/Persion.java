dfdd2befac80460457311fc0e7f13abbf163d707
风居住的巷口 <8379989+the-alley-where-the-wind-lives@user.noreply.gitee.com>
2024-03-20 16:28:55 +0800

    修改

--- src/day_240317/Extends/Persion.java
+++ src/day_240317/Extends/Persion.java
@@ -1,7 +1,7 @@
 package day_240317.Extends;
 
 public class Persion {
-    int age;
+    int age=18;
 
     public void sleep(){
         System.out.println("sleep...");
--- src/day_240317/Extends/Student.java
+++ src/day_240317/Extends/Student.java
@@ -5,7 +5,19 @@ import day_240317.Extends.Persion;
 public class Student extends Persion {
     int high;
 
-    public void eat(){
+    public void eat() {
         System.out.println("eat...");
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                "}";
+    }
+
+/*    @Override
+    public void sleep() {
+        System.out.println("Zzzzzzzz");
+    }*/
 }
