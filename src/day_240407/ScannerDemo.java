2d5d00ac1027db61f322ca8d82424a1499961998
rhythms1999 <1527325300@qq.com>
2024-04-07 14:40:58 +0800

    Scanner基础

---
+++ src/day_240407/ScannerDemo.java
@@ -0,0 +1,13 @@
+package day_240407;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String msg = input.next();
+
+        System.out.println(msg);
+    }
+}
