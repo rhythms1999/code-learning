1d09c0343623625c0a864b037da1757689dd3fe7
rhythms1999 <1527325300@qq.com>
2024-04-08 17:05:47 +0800

    情景2:||

--- src/day_240408/Situation.java
+++ src/day_240408/Situation.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Situation {
     public static void main(String[] args) {
-        System.out.println("情景1:");
+        System.out.println("情景2:");
         System.out.println("你老妈给你介绍对象...");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("输入对方性别：");
-        String sex = scanner.next().toLowerCase();
+        System.out.println("输入对方财富：");
+        int fortnue = scanner.nextInt();
 
         System.out.println("输入对方的年纪:");
         int herAge = scanner.nextInt();
 
-        if (sex.equals("woman") && herAge < 21) {
-            System.out.println("可以考虑相亲...");
-        }
-        else {
+        if (fortnue > 100000 || herAge < 30) {
+            System.out.println("抓紧相亲...");
+        } else {
             System.out.println("还是拜拜吧...");
         }
     }
