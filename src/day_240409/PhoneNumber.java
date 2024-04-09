ca4eacfdd50aa14ee6df86c95e6d44211d579b1a
rhythms1999 <1527325300@qq.com>
2024-04-09 10:34:29 +0800

    输出电话号码-纠正

--- src/day_240409/PhoneNumber.java
+++ src/day_240409/PhoneNumber.java
@@ -5,9 +5,7 @@ public class PhoneNumber {
         int arr1[] = new int[]{8, 2, 1, 0, 3};
         int arr2[] = new int[]{2, 0, 3, 2, 4, 0, 1, 3, 2, 3, 3};
 
-        for (int i = 0; i <= arr2.length; i++) {
-            // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 11 out of bounds for length 11
-            //	at day_240409.PhoneNumber.main(PhoneNumber.java:9) 数组下标越界错误
+        for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr1[arr2[i]]);
         }
     }
