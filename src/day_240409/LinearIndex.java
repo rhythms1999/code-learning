e8f540e22479ec4f67e14894efd295551d791f83
rhythms1999 <1527325300@qq.com>
2024-04-09 20:19:27 +0800

    线性查找

---
+++ src/day_240409/LinearIndex.java
@@ -0,0 +1,21 @@
+package day_240409;
+
+public class LinearIndex {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{10, 50, 30, 70, 80, 60, 20, 90, 40};
+        int target = 20;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("找到了目标数字:" + target + ",它处于第" + (i + 1) + "个位置");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("没有找到哦");
+        }
+    }
+}
