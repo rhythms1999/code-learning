424dde2b0d60452a949dbd44926a725826f97563
rhythms1999 <1527325300@qq.com>
2024-04-09 10:42:28 +0800

    输出电话号码-andmyself

--- src/day_240409/PhoneNumber.java
+++ src/day_240409/PhoneNumber.java
@@ -8,5 +8,14 @@ public class PhoneNumber {
         for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr1[arr2[i]]);
         }
+
+        System.out.println(" ");
+
+        int arr3[] = new int[]{3,0,5,2,1,8,6,9};
+        int arr4[] = new int[]{4,0,6,5,2,2,7,1,6,0,3};
+
+        for (int j = 0; j < arr2.length; j++) {
+            System.out.print(arr3[arr4[j]]);
+        }
     }
 }
