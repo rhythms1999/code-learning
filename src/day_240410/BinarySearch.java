d29cb8b9a16ac292c7ec7571316c107bf7b2f8b2
rhythms1999 <1527325300@qq.com>
2024-04-10 16:30:20 +0800

    二分法查找-新增情况、优化冗余

--- src/day_240410/BinarySearch.java
+++ src/day_240410/BinarySearch.java
@@ -5,22 +5,26 @@ public class BinarySearch {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 
         int flag = 0;
+        boolean found = false;
 
         int first = 0;
         int last = arr.length - 1;
-        int mid = (first + last) / 2;
 
         while (first <= last) {
+
+            int mid = (first + last) / 2;
+
             if (arr[mid] > flag) {
                 last = mid - 1;
-                mid = (first + last) / 2;
             } else if (arr[mid] < flag) {
                 first = mid + 1;
-                mid = (first + last) / 2;
             } else {
                 System.out.println("找到了目标数字flag=" + flag + "它在第" + (mid + 1) + "个位置上");
                 break;
             }
+        }
+        if (!found) {
+            System.out.println("没找到flag：" + flag);
         }
     }
 }
