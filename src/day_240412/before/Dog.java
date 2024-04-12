7b1dbef5bf5bc02b0f1a9795683019f2cf5312f0
rhythms1999 <1527325300@qq.com>
2024-04-12 19:35:33 +0800

    参考版本-修改前

---
+++ src/day_240412/before/Dog.java
@@ -0,0 +1,6 @@
+package day_240412.before;
+
+public class Dog {
+    String name;
+    String type;
+}
---
+++ src/day_240412/before/Student.java
@@ -0,0 +1,10 @@
+package day_240412.before;
+
+public class Student {
+    String name;
+    char sex;
+    String hair;
+    int age;
+
+    Dog[] dogs;
+}
